e61801a81037719196966a19100f8f1953110f92
Anthonio Tokinantenaina <98169662+Tokinantenaina-Nt@users.noreply.github.com>
2023-12-31 19:21:22 +0300

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div className="logo">
-            <img src="../src/assets/logo.png" alt="logo react" />
+            <img src="./../src/assets/logo.png" alt="logo react" />
             <h3>React World</h3>
         </div>
     );
